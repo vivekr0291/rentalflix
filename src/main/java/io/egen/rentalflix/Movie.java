59a3c6d64141fe54580e7ea5bc2ec411a6c7b38b
vivekr0291 <vivekr.aiesec@gmail.com>
2016-02-13 20:16:49 -0500

    homework1-13feb

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -5,5 +5,62 @@ package io.egen.rentalflix;
  * Fields: id, title, year, language
  */
 public class Movie {
+
 	//POJO IMPLEMENTATION GOES HERE
+	private int id;
+	private String title;
+	private int year;
+	private String language;
+	private int duration;
+
+	public void setID(int setid)
+	{
+	    id = setid;
+	}
+
+	public void setTitle(String settitle)
+	{
+	    title = settitle;
+	}
+
+	public void setYear(int y)
+	{
+	    year = y;
+	}
+
+	public void setLang(String setlang)
+	{
+	    lang = setlang;
+	}
+
+	public void setDuration(int setduration)
+	{
+	    duration = setduration;
+	}
+
+	public int getID()
+	{
+	    return id;
+	}
+
+	public String getTitle()
+	{
+	    return title;
+	}
+
+	public int getYear()
+	{
+	    return year;
+	}
+
+	public String getLang()
+	{
+	    return lang;
+	}
+
+	public int getDuration()
+	{
+	    return duration;
+	}
+
 }
--- src/main/java/io/egen/rentalflix/MovieService.java
+++ src/main/java/io/egen/rentalflix/MovieService.java
@@ -4,6 +4,74 @@ package io.egen.rentalflix;
  * Service implementing IFlix interface
  * You can use any Java collection type to store movies
  */
-public class MovieService implements IFlix {
+public class MovieService implements IFlix implements Runnable {
+    List<Movie> movieList;
+    Map<Integer, String> rentHistory = new HashMap<Integer, String>();
 
+    public List<Movie> findAll()
+    {
+        return movieList;
+    }
+
+    public List<Movie> findByName (String name)
+    {
+        List<Movie> listByName = new ArrayList<Movie>();
+        for(Movie m : movieList)
+        {
+            if(m.getTitle().toLowerCase().contains(name.toLowerCase()))
+            {
+                listByName.add(m);
+            }
+        }
+
+        return listByName;
+    }
+
+    public Movie create (Movie movie)
+    {
+        moveList.add(movie);
+        return movie;
+    }
+
+    public Movie update (Movie movie)
+    {
+       for(Movie m : movieList)
+       {
+           if(m.getID() == movie.getID)
+           {
+               movieList.remove(m);
+               movieList.add(movie);
+               return ;
+           }
+
+       }
+       throw new IllegalArgumentException("Illegal Argument");
+    }
+
+    public Movie delete (int id)
+    {
+        for(Movie m : movieList)
+       {
+           if(m.getID() == id)
+           {
+               movieList.remove(m);
+               return m;
+           }
+
+       }
+       throw new IllegalArgumentException("Illegal Argument");
+    }
+
+    public boolean rentMovie (int movieId, String user)
+    {
+        if(rentHistory.containsKey(movieId))
+        {
+            throw new IllegalArgumentException("Illegal Argument");
+            return false;
+        }
+
+        rentHistory.put(movieId, user);
+        return true;
+
+    }
 }
